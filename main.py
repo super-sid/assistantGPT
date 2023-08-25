dc78fce0a8e0b231ed8c829d367b11d7a59a3a81
Dhruvil Thaker <thaker.dhruvil@tifin.com>
2023-08-25 18:01:50 +0530

    Added chatlit

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-from json import tool
+# Imports
+import chainlit as cl
+from langchain.agents import initialize_agent, AgentType
 from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain import PromptTemplate, LLMChain
+from langchain import ConversationChain, PromptTemplate, LLMChain
 from langchain.agents.tools import Tool
-from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
-from langchain.agents.tools import Tool
-from langchain import LLMChain
-                                 
-llm = Ollama(base_url="http://127.0.0.1:11434", 
-             model="llama2", 
-             callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]),
-             verbose=True
-             )
+from langchain.memory import ConversationBufferMemory
 
-template = """USER: {question}
-ASSISTANT: You are a coding expert who provides all the necessary code for the requirements."""
-prompt = PromptTemplate(template=template, input_variables=["question"])
+# Setup
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-llm_chain = LLMChain(prompt=prompt,llm=llm, verbose=True)
+llm = Ollama(
+    base_url="http://127.0.0.1:11434", 
+    model="llama2", 
+    temperature=0
+)
 
-tools = [
-    Tool(
-        name="Programmer",
-        func=llm_chain.run,
-        description="You are a coding expert who provides all the necessary code for the requirements."
-    ),
-]
+template = """As an experienced software engineer, I understand the importance of structured projects. Given the requirement, provide a detailed folder and file structure first. Then, elaborate with the necessary code, comments, and best practices. If using specific frameworks or libraries, mention them along with their purposes.
 
-planner = load_chat_planner(llm)
-executor = load_agent_executor(llm, tools, verbose=True)
-agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
-agent.run("How to create a basic blog apis with python?")
+Question: {question}
+Answer: Let's think step by step."""
+
+@cl.on_chat_start
+def main():
+    # Instantiate the chain for that user session
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
+
+    # Store the chain in the user session
+    cl.user_session.set("llm_chain", llm_chain)
+
+@cl.on_message
+async def main(message: str):
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+
+    # Call the chain asynchronously
+    res = await cl.make_async(llm_chain)(
+        message, callbacks=[cl.LangchainCallbackHandler()]
+    )
+
+    # Do any post processing here
+
+    # "res" is a Dict. For this chain, we get the response by reading the "text" key.
+    # This varies from chain to chain, you should check which key to read.
+    await cl.Message(content=res["text"]).send()
+    return llm_chain
