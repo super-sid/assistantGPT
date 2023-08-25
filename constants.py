3a27a77a5e1df79896d3bb596ecfabe29baaab64
Dhruvil Thaker <thaker.dhruvil@tifin.com>
2023-08-25 21:09:57 +0530

    added file generation

---
+++ constants.py
@@ -0,0 +1,43 @@
+DESCRIPTION = "You are an agent to create boilerplate code in any programming language for a given project idea."
+
+GENERATE_PROJECT_STRUCTURE_TEMPLATE = template = """{description}
+
+The git repository is empty and you need to create a list of files required for the project idea. Include all relevant files usually found in a git repository.
+
+The list should be a `tree -ifF` output. Prefix each line with '-' character. Only include file names. You must be clear and concise. No explanations required. Make sure you do not include any irrelevant files or empty directories.
+
+Project idea: {project_idea}
+
+Output:"""
+
+UPDATE_PROJECT_STRUCTURE_TEMPLATE = """{description}
+
+Project idea: {project_idea}
+
+Project structure:
+{project_structure}
+
+You have to update the project structure based on the following instructions:
+
+Add Unit tests: {unit_tests}
+Add Docker support: {dockerization}
+Add Github Actions: {github_actions}
+Add Pre-commit hooks: {pre_commit_hooks}
+
+The list should be a `tree -ifF` output. Prefix each line with '-' character. You must be clear and concise. No explanations required.
+
+Project structure:"""
+
+PROJECT_FILE_TEMPLATE = """{description}
+
+Project idea: {project_idea}
+
+Project structure:
+{project_structure}
+
+You have to create file content based on the following project structure. The file content MUST be related to the project idea and structure.
+
+ONLY create file content for the file name specified below. You must be clear and concise. No explanations required.
+
+File name: {file_name}
+File content:"""
--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 # Imports
+import concurrent.futures
+import logging
 import chainlit as cl
-from langchain.agents import initialize_agent, AgentType
 from langchain.llms import Ollama
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain import ConversationChain, PromptTemplate, LLMChain
-from langchain.agents.tools import Tool
+from langchain import PromptTemplate, LLMChain
 from langchain.memory import ConversationBufferMemory
+from pathlib import Path
 
+import yaml
+from constants import *
 # Setup
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
+logger = logging.getLogger(__name__)
 llm = Ollama(
     base_url="http://127.0.0.1:11434", 
     model="llama2", 
     temperature=0
 )
 
-template = """As an experienced software engineer, I understand the importance of structured projects. Given the requirement, provide a detailed folder and file structure first. Then, elaborate with the necessary code, comments, and best practices. If using specific frameworks or libraries, mention them along with their purposes.
-
-Question: {question}
-Answer: Let's think step by step."""
-
 @cl.on_chat_start
 def main():
     # Instantiate the chain for that user session
-    prompt = PromptTemplate(template=template, input_variables=["question"])
+    prompt = PromptTemplate(template=template,
+        input_variables=[
+            "project_idea",
+        ],
+        partial_variables={"description": DESCRIPTION})
     llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
     # Store the chain in the user session
@@ -35,11 +35,23 @@ def main():
 async def main(message: str):
     # Retrieve the chain from the user session
     llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+    answer_prefix_tokens=["FINAL", "ANSWER"]
 
     # Call the chain asynchronously
     res = await cl.make_async(llm_chain)(
-        message, callbacks=[cl.LangchainCallbackHandler()]
+        message, callbacks=[cl.LangchainCallbackHandler(stream_final_answer=True,
+        answer_prefix_tokens=answer_prefix_tokens,)]
     )
+
+    chain_output = llm_chain.predict(project_idea=res)
+    # print("askjdnjasdnjasd", chain_output)
+    project_structure = yaml.safe_load(chain_output.strip())
+    # print("sahdiuashdaihsd", project_structure)
+    # cache the project structure
+    _write_file(
+        ".boilerplate_x", yaml.safe_dump(project_structure)
+    )
+    generate_project_files(llm_chain, res, project_structure)
 
     # Do any post processing here
 
@@ -47,3 +59,44 @@ async def main(message: str):
     # This varies from chain to chain, you should check which key to read.
     await cl.Message(content=res["text"]).send()
     return llm_chain
+
+def generate_project_files(llm_chain, prompt, project_structure: list[str]) -> None:
+    """Generates the project files."""
+    # print("asdasdasdasd", llm_chain, prompt, project_structure)
+    project_structure_str = yaml.safe_dump(project_structure)
+    print("asdasdasd", project_structure)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for file_name in project_structure:
+            print("asjndijajindasd", file_name)
+            if (Path("Fastapi") / file_name).exists():
+                logger.info(f"File already exists: {file_name}")
+                continue
+            if (Path("Fastapi") / file_name).suffix == "":
+                logger.info(f"Skipping directory: {file_name}")
+                continue
+            logger.info(f"Generating file content: {file_name}...")
+            futures.append(
+                executor.submit(
+                    generate_project_file, llm_chain, prompt, file_name, project_structure_str
+                )
+            )
+        for future in concurrent.futures.as_completed(futures):
+            if future.exception() is not None:
+                logger.error(f"Failed to generate file: {future.exception()}")
+
+def generate_project_file(llm_chain, prompt, file_name: str, project_structure_str: str
+    ) -> None:
+        """Generates a single project file."""
+        file_content = llm_chain.predict(
+            project_idea=prompt,
+            project_structure=project_structure_str,
+            file_name=file_name,
+        )
+        _write_file(file_name, file_content)
+
+def _write_file(file_name: str, file_content: str):
+    """Writes the file to the output path."""
+    file_path = Path("Fastapi") / file_name
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    file_path.write_text(file_content)
