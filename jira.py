74c9dcc7d3ff5d75887aa7d9979cb868cc1ffec0
Siddhant Agnihotri <siddhant.ag7@gmail.com>
2023-08-29 15:44:47 +0530

    Made modifications for Jira Ops

--- jira.py
+++ jira.py
@@ -1,31 +1,33 @@
 import requests
 import json
 
+access_token = "ATATT3xFfGF0emz_aSBPPcbmZIotwLiJj14mf_bCLpua1Vj3csoTIHdQIZJkKa1QMGN5_yEX4v48aVhmszVjD8DXRhBvtt0m_HyzfMUF9uoOPKM6a8Z5yIewrXctLB6WwJzRH0SyjF39Seb6l0nfOtD7NfZ-MOuE-9-a0n_WPnYzALqqYMd0Cm0=2C32570A"
+user_email = "ashwin.manohar@tifin.com"
+
 
 def create_jira_ticket(ticket_data_array):
     url = "https://hackathon-test.atlassian.net/rest/api/2/issue/"
-    headers = {
-        "Accept": "application/json",
-        "Content-type": "application/json"
-    }
+    headers = {"Accept": "application/json", "Content-type": "application/json"}
     tickets = json.loads(ticket_data_array).get("tasks")
     for ticket_data in tickets:
-            payload = json.dumps(
-                {
-                    "fields": {
-                        "project":
-                        {
-                            "key": "HAC"
-                        },
-                        "summary": ticket_data["name"],
-                        "description": ticket_data["description"],
-                        "issuetype": {
-                            "name": "Task"
-                        },
-                    }
+        payload = json.dumps(
+            {
+                "fields": {
+                    "project": {"key": "HAC"},
+                    "summary": ticket_data["name"],
+                    "description": ticket_data["description"],
+                    "issuetype": {"name": "Task"},
                 }
-            )
+            }
+        )
 
-            response = requests.post(url, headers=headers, data=payload, auth=("ashwin.manohar@tifin.com",
-                                                                       "ATATT3xFfGF0De2KuzeSj01yF0vYW9DmRdSHhbRHeVFhagfY2WxH8YJn8tdFTXNIZrR5jr-UEt_L3fDeD8VQqdLmlYUXMsrygObuLxCu-TbG3VQAK3ZoSXUH880eQDpoOXH7bZ0Tbf6hZlmtK6YZAW9w42BjKluxZJ4NzmQUJHElflQEn2hokYM=984CF0AA"))            
-            print(response.text)
+        response = requests.post(
+            url,
+            headers=headers,
+            data=payload,
+            auth=(
+                user_email,
+                access_token,
+            ),
+        )
+        print(response.text)
